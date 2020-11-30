555b6449c83a1d6510590a1998b121ce0051b196
DucLee <funfox2906@gmail.com>
2020-11-30 13:53:53 +0100

    delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def deleteCidInText(text):
-    index = text.find("cid") - 2
-    text = text[:index] 
-    text +=" }\""
-    return text
-
-text = "h bài này lắm, cid: Ugz4S7HQPQdAhsxLHKt4AaABAg}"
-print(deleteCidInText(text))
